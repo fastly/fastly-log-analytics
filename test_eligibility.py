0ab14cf725a50ea5137fa1500723617dd0bccd8f
Drew Michael <dmichael@fastly.com>
2026-09-17 18:42:24 -0600

    chore: remove test files

--- test_eligibility.py
+++
@@ -1,6 +0,0 @@
-from datetime import UTC, datetime, timedelta
-
-st = datetime(2026, 9, 16, 21, 19, 28, 594000, tzinfo=UTC)
-et = datetime(2026, 9, 17, 21, 19, 28, 594000, tzinfo=UTC)
-print(f"et - st = {et - st}")
-print(f"min_hours check (et - st) < 24h: {(et - st) < timedelta(hours=24)}")
